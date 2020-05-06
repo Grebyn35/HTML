99de1edfd4f35ec4e752df69082f0a4009b13417
Elliot Nyberg <55945978+Grebyn35@users.noreply.github.com>
2020-05-06 22:04:28 +0200

    Add files via upload

--- HTML & Css/Uppgifter/JS/myjquery.js
+++ HTML & Css/Uppgifter/JS/myjquery.js
@@ -1,8 +1,6 @@
-				$(document).ready(function(){
+$(document).ready(function(){
 		$('.menu-toggle').click(function(){
 		$('.menu-toggle').toggleClass('active')
 		$('nav').toggleClass('active')
 		});
-		});
-	
-	
+		})
